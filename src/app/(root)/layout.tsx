c6a83e014b67c76e7e11f0219b1c0eff4d7b6d50
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-05-11 21:04:03 +0530

    implementing sidebar, mobile header and navbar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
 
 const layout = ({ children } : { children: React.ReactNode }) => {
   return (
     <main className='root'>
-        <div className='root-container'>
-            <div className='wrapper'>
-                {children}
-            </div>
-        </div>
+      <Sidebar />
+      <MobileNav />
+      <div className='root-container'>
+          <div className='wrapper'>
+              {children}
+          </div>
+      </div>
         
     </main>
   )
